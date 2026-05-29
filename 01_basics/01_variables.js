a5bca625b2c4886aa7de4914b28dbe5bb0dca86c
Bhumik Sharma <bhumikkannu@gmail.com>
2026-05-29 15:16:42 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "bhumik@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountBalance
+
+//do not use var , bcoz of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountBalance])
+
+console.log(accountId);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+2) //we are using nodejs, not browser
+
+console.log(3+2)
+//code readability should be high
+console.log("Bhumik")
+//tc39,mdn,ecma script 
+
+let name = "Bhumik"
+let age = 18
+let isLoggedIn = false //boolean
+
+//number => 2 to power 53 (range)
+//bigint
+//string => "", ''
+//boolean
+//null => standlone value
+//undefined => 
+//symbol => unique
+
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
