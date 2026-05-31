3f2b61db29f4b2b11140d887bb22f45d9be58ac0
Bhumik Sharma <bhumikkannu@gmail.com>
2026-05-31 12:09:16 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let Score = "33"
+console.log(typeof(Score));
+console.log(typeof Score);
+let convertedNumber = Number(Score);
+console.log(typeof convertedNumber)
+console.log(convertedNumber)
+
+//"33"=>33
+//ab33=>NaN
+//true=>1; false=>0
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//"hitesh"=>true
+//1=>true 0=>false
+//"hitesh"=>true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
