7cae2243405838d4d75f9a7b5683cfdbdd1a1e0d
Bhumik Sharma <bhumikkannu@gmail.com>
2026-06-13 16:42:34 +0000

    Commit dataTypes summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+let Number = 6
+console.log(Number>=6);
+let ok = null
+console.log(ok>=0);
+//== check and comparison checks (>,<,>=,<=) works differently
+//comparison converts null to a number 0
+//null >= 0 is true while null >0 is false
+console.log(null > 0);
+console.log(undefined == 0 || undefined >= 0 || undefined>0) ;
+
+
+
